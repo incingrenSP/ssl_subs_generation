73d6480f826b96346997cdf442602618f489c735
Samir Pokharel <samirpokharel002@gmail.com>
2025-10-21 23:14:32 +0545

    optimized pretraining encoder

--- src/requirements.py
+++ src/requirements.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.checkpoint import checkpoint
 import torch.nn.utils.rnn as rnn_utils
 
 from torch.profiler import profile, ProfilerActivity
